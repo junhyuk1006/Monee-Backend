217ba96d9afef437e85b62a90d8afffc6da8ca3d
junhyuk1006 <whdid3766@gmail.com>
2025-07-08 21:48:23 +0900

    생성

---
+++ src/test/java/com/example/moneebackend/MoneeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moneebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
